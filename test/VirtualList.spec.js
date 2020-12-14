2ae7de47019a6c7050cbdb9aa734450b0413f2b7
Skayo <jonas@skayo.dev>
2020-12-14 03:05:15 +0100

    Added VirtualList test file

---
+++ test/VirtualList.spec.js
@@ -0,0 +1,12 @@
+require('@testing-library/jest-dom/extend-expect');
+
+const { render /*, fireEvent*/ } = require('@testing-library/svelte');
+
+const VirtualList = require('../src/VirtualList.svelte');
+
+test('renders successfully', () => {
+	const { container } = render(VirtualList, { height: 600, itemCount: 1000, itemSize: 50 });
+	expect(container).toBeInTheDocument();
+});
+
+/* TODO: Add tests */
