ae952cefa20d3e26958ae53ab81a5780c31ffd26
Lion King917 <lionking917@outlook.com>
2024-02-13 00:39:18 -0800

    Update item, expand item styling

--- src/VirtualList.svelte
+++ src/VirtualList.svelte
@@ -359,10 +359,10 @@
 
 			if (sticky) {
 				style += `position:sticky;flex-grow:0;z-index:1;top:0;margin-top:${offset}px;margin-bottom:${-(offset + size)}px;`;
-				expandStyle += `position:sticky;flex-grow:0;z-index:1;top:0;margin-top:${expandOffset}px;margin-bottom:${-(expandOffset + expandSize)}px;overflow: hidden;`;
+				expandStyle += `position:sticky;flex-grow:0;z-index:1;top:0;margin-top:${expandOffset}px;margin-bottom:${-(expandOffset + expandSize)}px;`;
 			} else {
 				style += `position:absolute;top:${offset}px;`;
-				expandStyle += `position:absolute;top:${expandOffset}px;overflow: hidden;`;
+				expandStyle += `position:absolute;top:${expandOffset}px;`;
 			}
 		} else {
 			style = `top:0;width:${size}px;`;
@@ -370,10 +370,10 @@
 
 			if (sticky) {
 				style += `position:sticky;z-index:1;left:0;margin-left:${offset}px;margin-right:${-(offset + size)}px;`;
-				expandStyle += `position:sticky;z-index:1;left:0;margin-left:${expandOffset}px;margin-right:${-(expandOffset + expandSize)}px;overflow: hidden;`;
+				expandStyle += `position:sticky;z-index:1;left:0;margin-left:${expandOffset}px;margin-right:${-(expandOffset + expandSize)}px;`;
 			} else {
 				style += `position:absolute;height:100%;left:${offset}px;`;
-				expandStyle += `position:absolute;height:100%;left:${expandOffset}px;overflow: hidden;`;
+				expandStyle += `position:absolute;height:100%;left:${expandOffset}px;`;
 			}
 		}
 
@@ -390,21 +390,25 @@
 	{#if mode === WRAPPER_MODE.DIV}
 		<div class="virtual-list-inner" style={innerStyle}>
 			{#each visibleItems as item (getKey ? getKey(item.index) : item.index)}
-				<slot name="item" item={items[item.index]} activeClass={expandItems[item.index] ? "active" : ""} style={item.style.style} index={item.index} />
+				<div style={item.style.style} class={expandItems[item.index] ? "active" : ""}>
+					<slot name="item" item={items[item.index]} index={item.index} style="height: 100%;" />
+				</div>
 				{#if expandItems[item.index]}
-					<slot name="expandItem" item={items[item.index]} style={item.style.expandStyle} index={item.index} />
+					<div style={item.style.expandStyle}>
+						<slot name="expandItem" item={items[item.index]} index={item.index} style="height: 100%; overflow: hidden;" />
+					</div>
 				{/if}
 			{/each}
 		</div>
 	{:else}
 		<table class="virtual-list-inner" style={innerStyle}>
 			{#each visibleItems as item (getKey ? getKey(item.index) : item.index)}
-				<tr style={item.style.style}>
-					<slot name="item" item={items[item.index]} activeClass={expandItems[item.index] ? "active" : ""} style="" index={item.index} />
+				<tr style={item.style.style} class={expandItems[item.index] ? "active" : ""}>
+					<slot name="item" item={items[item.index]} index={item.index} style="height: 100%;" />
 				</tr>
 				{#if expandItems[item.index]}
 					<tr style={item.style.expandStyle}>
-						<slot name="expandItem" item={items[item.index]} index={item.index} style="" />
+						<slot name="expandItem" item={items[item.index]} index={item.index} style="height: 100%; overflow: hidden;" />
 					</tr>
 				{/if}
 			{/each}
