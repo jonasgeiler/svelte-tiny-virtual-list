1439b78ba964e9487b99af711666c1b8405d5e05
Jonas Geiler <git@jonasgeiler.com>
2024-04-22 23:02:13 +0200

    text: fixed integration test

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 // TODO: Add more integration tests
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected text from readme', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'svelte-tiny-virtual-list' })).toBeVisible();
+	await expect(page.getByText('A tiny but mighty list virtualization library, with zero dependencies 💪')).toBeVisible();
 });
