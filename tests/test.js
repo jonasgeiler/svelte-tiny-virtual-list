b317aa6105e251bcce73bdefde02a6cf2f8aebef
Jonas Geiler <git@jonasgeiler.com>
2024-04-29 15:36:40 +0200

    style: fix formatting issues with prettier

--- tests/test.js
+++ tests/test.js
@@ -6,5 +6,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected text from readme', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByText('A tiny but mighty list virtualization library, with zero dependencies 💪')).toBeVisible();
+	await expect(
+		page.getByText('A tiny but mighty list virtualization library, with zero dependencies 💪')
+	).toBeVisible();
 });
